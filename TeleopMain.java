225ed5cb15a6c6fd570a1ad6ec71006aa7008684
Endersky78 <36282715+Endersky78@users.noreply.github.com>
2018-11-17 10:26:32 -0800

    Update TeleopMain.java
    Added support for continuous rotation servos

--- TeleopMain.java
+++ TeleopMain.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="TeleopMain", group="Iterative Opmode")
@@ -14,6 +15,8 @@ public class TeleopMain extends OpMode
     public DcMotor backLeftW = null;
     public DcMotor backRightW = null;
     public DcMotor arm = null;
+    public CRServo wristL = null;
+    public CRServo wristR = null;
 
     private ElapsedTime runtime = new ElapsedTime();
 
@@ -26,11 +29,15 @@ public class TeleopMain extends OpMode
         backLeftW = hardwareMap.get(DcMotor.class, "BL");
         backRightW = hardwareMap.get(DcMotor.class, "BR");
         arm = hardwareMap.get(DcMotor.class, "arm");
+        wristL = hardwareMap.get(CRServo.class, "WL");
+        wristR = hardwareMap.get(CRServo.class, "WR");
 
         frontLeftW.setDirection(DcMotor.Direction.REVERSE);
         backLeftW.setDirection(DcMotor.Direction.REVERSE);
         frontRightW.setDirection(DcMotor.Direction.FORWARD);
         backRightW.setDirection(DcMotor.Direction.FORWARD);
+        wristL.setDirection(DcMotor.Direction.FORWARD);
+        wristR.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.addData("Status", "Initialized");
     }
@@ -54,7 +61,29 @@ public class TeleopMain extends OpMode
         backLeftW.setPower(leftPower*0.75);
         frontRightW.setPower(rightPower*0.75);
         backRightW.setPower(rightPower*0.75);
-        arm.setPower(gamepad1.left_trigger*0.5);
+        if(gamepad1.left_trigger > 0) {
+            arm.setPower(-gamepad1.left_trigger*0.25);
+        }
+
+        else if(gamepad1.right_trigger > 0) {
+            arm.setPower(gamepad1.right_trigger*0.25);
+        }
+
+        else if(gamepad1.a) {
+            wristL.setPower(0.5);
+            wristR.setPower(0.5);
+        }
+
+        else if(gamepad1.y) {
+            wristL.setPower(-0.5);
+            wristR.setPower(-0.5);
+        }
+
+        else {
+            wristL.setPower(0);
+            wristR.setPower(0);
+            arm.setPower(-0.05);
+        }
 
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
